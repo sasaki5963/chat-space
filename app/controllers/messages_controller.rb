bb36b97164b1e2b24b6f457b397a126acfd585b0
sasaki5963 <2rg38651135142s@ezweb.ne.jp>
2020-07-20 17:13:13 +0900

    ChatSpace MS送信機能の実装完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   before_action :set_group
 
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
@@ -26,4 +27,5 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
+
 end
